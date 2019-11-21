a6ac0b51d4223d0532e8cdae2e2a92db3abdbd7e
elitx10 <thomas.lescolan@sfr.fr>
2019-11-21 15:00:51 +0100

    add festival MS and Spring cloud config edge MS

---
+++ ms-festival/src/test/java/com/festival/msfestival/MsFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festival.msfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
