36685ffac0d11f47e99f783f82c7b30c696c0490
elitx10 <thomas.lescolan@sfr.fr>
2019-11-26 10:09:17 +0100

    add zuul server

--- zuul-server/src/main/java/com/festival/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/festival/zuulserver/ZuulServerApplication.java
@@ -1,17 +1,19 @@
-package com.festival.eurekaserver;
+package com.festival.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableZuulProxy
+@EnableDiscoveryClient
 @EnableConfigurationProperties
-public class EurekaServerApplication {
+public class ZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
 }
