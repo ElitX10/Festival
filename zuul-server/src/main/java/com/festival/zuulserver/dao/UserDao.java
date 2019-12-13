add3be846a4d7ea63cd792afe64e6a9d7cc7f66b
elitx10 <thomas.lescolan@sfr.fr>
2019-12-13 15:27:10 +0100

    Add user Auth system

---
+++ zuul-server/src/main/java/com/festival/zuulserver/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.festival.zuulserver.dao;
+
+import com.festival.zuulserver.models.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+    DAOUser findByUsername(String username);
+}
